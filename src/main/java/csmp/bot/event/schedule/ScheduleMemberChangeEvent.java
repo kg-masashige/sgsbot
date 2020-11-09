4262eec1a34c36ee4c3b8f67b02aac4a58abf021
MASASHIGE <kg.masashige@gmail.com>
2020-11-10 06:57:32 +0900

    取得できていたため再度修正。

--- src/main/java/csmp/bot/event/schedule/ScheduleMemberChangeEvent.java
+++ src/main/java/csmp/bot/event/schedule/ScheduleMemberChangeEvent.java
@@ -40,11 +40,6 @@ public class ScheduleMemberChangeEvent implements IDiscordEvent {
 		// デイコードから情報を取得する。
 		String guildId = ded.getGuild().getIdAsString();
 
-		// TODO Discord障害が修正されるまで機能を止める
-		if (guildId != null) {
-			return;
-		}
-
 		if (!checkGuildId(guildId)) {
 			return;
 		}
